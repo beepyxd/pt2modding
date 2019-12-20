4f5ff3b48d7ef65c204c2ef758ce98905cbfd911
realBeeps <33036552+beepyxd@users.noreply.github.com>
2019-12-20 14:41:29 +0100

    Update error_checker.js

--- js/error_checker.js
+++ js/error_checker.js
@@ -6,14 +6,14 @@ module.exports = {
   check: function check_errors(path1, paths, multiplying, path_num) {
 	path1=path1.replace(/(?:\x2c\x3b|\x2c|\x3b)/g, ','); paths=paths.replace(/(?:\x2c\x3b|\x2c|\x3b)/g, ',');
 	paths=paths.split(",");
-	result=[];
-	lastEl=paths.pop();
-	//if (lastEl!=="") {result.push(lastEl)}
+	var result=[];
+	var lastEl=paths.pop();
+	if (lastEl!=="") {result.push(lastEl)}
 	for (i=0; i<paths.length; i++) {
 		if (paths[i].includes("2<>")||paths[i].includes("3<>")||paths[i].includes("5<>")||paths[i].includes("6<>")||paths[i].includes("7<>")||paths[i].includes("8<>")||paths[i].includes("9<>")||paths[i].includes("10<>")) {result.push(paths[i])}
 	}
 	for (i=0; i<paths.length; i++) {
-		a1=paths[i].indexOf("<"); a2=paths[i].lastIndexOf(">");
+		var a1=paths[i].indexOf("<"); var a2=paths[i].lastIndexOf(">");
 		if (a2==paths[i].length-1) {paths[i]=paths[i].substr(0, paths[i].length-1)}
 		if (a1!=-1) {paths[i]=paths[i].substr(a1+1, paths[i].length)}
 	}
@@ -26,7 +26,7 @@ module.exports = {
 		if (a3_match==null) {a3_match=")"}
 		a4=paths[i].indexOf("["); a4_match=paths[i].match(/\x5b/g);
 		if (a4_match==null) {a4_match="["}
-		a_match=a1_match+a2_match+a3_match+a4_match; a4=a4-1;
+		var a_match=a1_match+a2_match+a3_match+a4_match; a4=a4-1;
 		if (paths[i].indexOf("(")>0) {result.push(paths[i])}
 		else if (paths[i].includes("\"")||paths[i].includes("\'")||paths[i].includes("*")||paths[i].includes("?")||paths[i].includes("|")||paths[i].includes("+")||paths[i].includes("_")||paths[i].includes("=")||paths[i].includes(":")||paths[i].includes("`")||paths[i].includes("\\")||paths[i].includes("Z")||paths[i].includes("z")||paths[i].includes("Q[")||paths[i].includes("R[")||paths[i].includes("S[")||paths[i].includes("T[")||paths[i].includes("U[")||paths[i].includes("V[")||paths[i].includes("W[")||paths[i].includes("X[")||paths[i].includes("Y[")) {result.push(paths[i])}
 		else if (paths[i].includes("\r")||paths[i].includes("\n")) {result.push("used enter")}
@@ -39,8 +39,8 @@ module.exports = {
 		else if (a4==-2&&paths[i].includes("]")) {result.push(paths[i])}
 		else if (a_match.match(/\x28/g).length>1||a_match.match(/\x29/g).length>1||a_match.match(/\x5b/g).length>1||a_match.match(/\x5d/g).length>1) {result.push(paths[i])}
 		else {
-			a5=paths[i].indexOf("{"); a6=paths[i].indexOf("}"); a6=a6-2;
-			effects=paths[i].substr(a5, a6+2);
+			var a5=paths[i].indexOf("{"); var a6=paths[i].indexOf("}"); a6=a6-2;
+			var effects=paths[i].substr(a5, a6+2);
 			if (a5!=a6&&a5>-1&&a6>-1) {result.push(paths[i])}
 			if (a5==-1&&a6==-3) {effects="{1}"}
 			if (effects==="{1}") {paths[i]=paths[i].substr(0, a5)+paths[i].substr(a6+3, paths[i].length)}
@@ -93,16 +93,15 @@ module.exports = {
 					}
 				}
 				if (spaces(paths[i])==false) {paths[i]="a[M]"}
-				tiles_start=paths[i].indexOf("[");
+				var tiles_start=paths[i].indexOf("[");
 				tiles_start=tiles_start+1;
-				tiles_length=paths[i].substr(tiles_start, paths[i].length);
+				var tiles_length=paths[i].substr(tiles_start, paths[i].length);
 				tiles_length=tiles_length.substr(0, tiles_length.length-1);
 				if (tiles(tiles_length)==true) {result.push(paths[i])}
-				paths1=paths[i].substr(0, tiles_start-1);
+				var paths1=paths[i].substr(0, tiles_start-1);
 				if (paths1.substr(paths1.length-1, paths1.length)===")") {paths1=paths1.substr(0, paths1.length-1)}
 				if (paths1.indexOf("(")==0) {paths1=paths1.substr(1, paths1.length)}
-				paths_no_arp=paths1.replace(/\x21|\x24|\x25|\x26|\x2e|\x40|\x5e|\x7e/g, ',');
-				paths_no_arp=paths_no_arp.split(",");
+				var paths_no_arp=paths1.replace(/\x21|\x24|\x25|\x26|\x2e|\x40|\x5e|\x7e/g, ',').split(",");
 				var correct_notes = ["A-3", "#A-3", "B-3", "C-2", "#C-2", "D-2", "#D-2", "E-2", "F-2", "#F-2", "G-2", "#G-2", "A-2", "#A-2", "B-2", "C-1", "#C-1", "D-1", "#D-1", "E-1", "F-1", "#F-1", "G-1", "#G-1", "A-1", "#A-1", "B-1", "c", "#c", "d", "#d", "e", "f", "#f", "g", "#g", "a", "#a", "b", "c1", "#c1", "d1", "#d1", "e1", "f1", "#f1", "g1", "#g1", "a1", "#a1", "b1", "c2", "#c2", "d2", "#d2", "e2", "f2", "#f2", "g2", "#g2", "a2", "#a2", "b2", "c3", "#c3", "d3", "#d3", "e3", "f3", "#f3", "g3", "#g3", "a3", "#a3", "b3", "c4", "#c4", "d4", "#d4", "e4", "f4", "#f4", "g4", "#g4", "a4", "#a4", "b4", "c5", "mute", "empty"];
 				for (j=0; j<paths_no_arp.length; j++) {
 					if (spaces(paths_no_arp[j])==true) {if (correct_notes.indexOf(paths_no_arp[j])==-1) {result.push(paths[i]); break;}}
@@ -110,14 +109,14 @@ module.exports = {
 			}
 		}
 	}
-	b1=path1.match(/\x3c/g);
+	var b1=path1.match(/\x3c/g);
 	if (b1==null) {b1=""}
-	b2=path1.match(/\x3e/g);
+	var b2=path1.match(/\x3e/g);
 	if (b1.length>0&&b2==null) {
 		path1="5<a[M]>,5<a[M]>";
 		result.push(`<${result}`);
 	}
-	special_tiles=[];
+	var special_tiles=[];
 	path1=path1.split(",");
 	for (i=0; i<path1.length; i++) {
 		j=i+1;
@@ -139,7 +138,7 @@ module.exports = {
 		}
 	}
 	if (special_tiles=="") {special_tiles=["5<a[M]>", "5<a[M]>"]}
-	special_tiles1=special_tiles.pop();
+	var special_tiles1=special_tiles.pop();
 	special_tiles1=special_tiles1.substr(0, special_tiles1.length-1);
 	special_tiles.push(special_tiles1);
 	special_tiles1=special_tiles.shift();
@@ -153,21 +152,15 @@ module.exports = {
 	special_tiles=special_tiles.replace(/\x3e\x2c2\x3c|\x3e\x2c3\x3c|\x3e\x2c5\x3c|\x3e\x2c6\x3c|\x3e\x2c7\x3c|\x3e\x2c8\x3c|\x3e\x2c9\x3c|\x3e\x2c10\x3c/g, ",");
 	special_tiles=special_tiles.split(",");
 	path1=path1.join();
-	path1_return=path1.split(",");
-	path1=path1.replace(/1|2|3|4|5|6|7|8|9|0|\x21|\x23|\x24|\x25|\x26|\x28|\x29|\x40|\x2d|\x2e|\x3c|\x3e|\x5b|\x5d|\x5e|\x7b|\x7d|\x7e|mute|empty|A|B|C|D|E|F|G|Z|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z/g, "");
-	path1=path1.replace(/Q/g, "RR"); path1=path1.replace(/R/g, "SS"); path1=path1.replace(/S/g, "TT"); path1=path1.replace(/T/g, "UU"); path1=path1.replace(/U/g, "VV"); path1=path1.replace(/V/g, "WW"); path1=path1.replace(/W/g, "XX"); path1=path1.replace(/X/g, "YY"); path1=path1.replace(/Y/g, "P");
-	path1=path1.replace(/H/g, "II"); path1=path1.replace(/I/g, "JJ"); path1=path1.replace(/J/g, "KK"); path1=path1.replace(/K/g, "LL"); path1=path1.replace(/L/g, "MM"); path1=path1.replace(/M/g, "NN"); path1=path1.replace(/N/g, "OO"); path1=path1.replace(/O/g, "PP");
-	path1=path1.split(",");
+	var path1_return=path1.split(",");
+	path1=path1.replace(/1|2|3|4|5|6|7|8|9|0|\x21|\x23|\x24|\x25|\x26|\x28|\x29|\x40|\x2d|\x2e|\x3c|\x3e|\x5b|\x5d|\x5e|\x7b|\x7d|\x7e|mute|empty|A|B|C|D|E|F|G|Z|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z/g, "").replace(/Q/g, "RR").replace(/R/g, "SS").replace(/S/g, "TT").replace(/T/g, "UU").replace(/U/g, "VV").replace(/V/g, "WW").replace(/W/g, "XX").replace(/X/g, "YY").replace(/Y/g, "P").replace(/H/g, "II").replace(/I/g, "JJ").replace(/J/g, "KK").replace(/K/g, "LL").replace(/L/g, "MM").replace(/M/g, "NN").replace(/N/g, "OO").replace(/O/g, "PP").split(",");
 	path1.pop();
-	result1=[];
+	var result1=[];
 	if (multiplying!=0) {
 		const path1_new=path1.map(({length}) => {
-			const x=length;
-			if (x==0) {return 1}
-			const length_of_tile=x*multiplying;
-			if (length_of_tile<1) {return -1}
-			const n=parseInt(length_of_tile);
-			return n;
+			if (length==0) {return 1}
+			if (length*multiplying<1) {return -1}
+			return parseInt(length*multiplying);
 		});
 		path1=path1_new;
 		for (i=0; i<=path1.length; i++) {
